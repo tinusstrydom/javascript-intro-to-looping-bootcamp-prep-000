44be39a9ed0fcbb56124384ace96f37cf81ff330
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-14 17:57:42 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,9 @@ function forLoop(array){
   const arr = [];
   const num = 25;
   for(let i =0; i < num; i++){
-    arr.push(`I am ${i} strange loop${i === 0 ? '' : 's'}`);
+    arr.push(`I am ${i} strange loop${i === 1 ? '' : 's'}`);
+    let first = "I am 1 strange loop."
+    let rest = "I am 24 strange loops."
   }
 
   return arr;
